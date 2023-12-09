9cf73ff154cfd9ef5e070bb6681438f5bde02eef
Isah Ohieku <isahohieku@gmail.com>
2023-12-09 08:10:23 +0100

    console app build on release

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const say = require('cowsay').say;
 
 const text = 'Congratulations! This app is superb.'
 
 console.log(say({ text }));
+console.log({build_number: process.env.APP_BUILD});
