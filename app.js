1abc46da1cd5d42bb509d15cccf2c342f47de4ce
Isah Ohieku <isahohieku@gmail.com>
2025-12-01 12:06:16 +0100

    Check germini presence

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const say = require('cowsay').say;
 
-const text = 'Congratulations! This app is superb.'
+const text = 'Congratulations! This app is seem to work correctly.'
 
 console.log(say({ text }));
